f041d19af15906ff926617b33f09525ad30fe78a
jiyeoniiiii <139126791+jiyeoniiiii@users.noreply.github.com>
2023-10-16 22:48:55 +0900

    Create ec2_snapshot_public_check.py

---
+++ aws_management/EC2/ec2_snapshot_public_check.py
@@ -0,0 +1,22 @@
+import boto3
+
+# AWS 자격 증명 설정
+aws_access_key_id = ''
+aws_secret_access_key = ''
+aws_region = 'ap-northeast-2'
+
+# Boto3 클라이언트 생성
+ec2_client = boto3.client('ec2', region_name=aws_region)
+
+# 모든 EBS 스냅샷 가져오기
+response = ec2_client.describe_snapshots(OwnerIds=['self'])
+
+# EBS 스냅샷을 반복하며 공개 여부 확인
+for snapshot in response['Snapshots']:
+    snapshot_id = snapshot['SnapshotId']
+    is_public = snapshot['Public']
+
+    if is_public:
+        print(f"[PASS] EBS 스냅샷 {snapshot_id}는 공개되어 있습니다.")
+    else:
+        print(f"[FAIL] EBS 스냅샷 {snapshot_id}는 비공개입니다.")
