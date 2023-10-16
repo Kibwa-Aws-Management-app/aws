0f03105ab41636b4898c13743b91e6120d068d94
jiyeoniiiii <139126791+jiyeoniiiii@users.noreply.github.com>
2023-10-16 22:49:14 +0900

    Create ec2_volume_default_encryption_check.py

---
+++ aws_management/EC2/ec2_volume_default_encryption_check.py
@@ -0,0 +1,20 @@
+import boto3
+
+# AWS 자격 증명 설정
+aws_access_key_id = ''
+aws_secret_access_key = ''
+aws_region = 'ap-northeast-2'
+
+# Boto3 클라이언트 생성
+ec2_client = boto3.client('ec2', region_name=aws_region)
+
+# 모든 EBS 볼륨 가져오기
+response = ec2_client.describe_volumes()
+
+# EBS 볼륨을 반복하며 기본 암호화 설정 여부 확인
+for volume in response['Volumes']:
+    volume_id = volume['VolumeId']
+    is_default_encryption = volume['KmsKeyId'] == "alias/aws/ebs"
+
+    if is_default_encryption:
+        print(f"[PASS] EBS 볼륨 {volume_id}는 기본 암호화가
