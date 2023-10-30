c057c90fca53016a6f214387e434a61d73e519df
garamkim83 <garamk83@gmail.com>
2023-10-30 23:14:31 +0900

    색상 변경

--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,3 @@
-
-// 아래는 예시 코드입니다. 실제로 사용하지 마세요.
-// Select the button element
-const button = document.querySelector('#myButton');
-
-// Attach an event listener to the button
-button.addEventListener('click', function () {
-    console.log('Button clicked!');
-});
-
-
 function start_home() {
     console.log('start_home');
     let main_content_title = document.querySelector('#main-content-title');
