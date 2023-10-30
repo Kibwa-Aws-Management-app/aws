2bc6893da98d423dbf45c8729bd5a1d99ed2a72a
garamkim83 <garamk83@gmail.com>
2023-10-30 22:12:07 +0900

    html, css 초안

---
+++ static/js/index.js
@@ -0,0 +1,9 @@
+
+// 아래는 예시 코드입니다. 실제로 사용하지 마세요.
+// Select the button element
+const button = document.querySelector('#myButton');
+
+// Attach an event listener to the button
+button.addEventListener('click', function() {
+    console.log('Button clicked!');
+});
